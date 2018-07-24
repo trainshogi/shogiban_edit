a32f11acdb3277f42c6368779b494758b02cd921
trainshogi <densyashogi@gmail.com>
2018-07-24 20:28:21 +0900

    Third commit
    必要最低限のapp.pyを作成。Flaskがあればpython app.pyで実行可能。HTMLはindex.htmlのみ。CSSはstyle.css。現状自作JSソースは不使用。

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Flask などの必要なライブラリをインポートする
+from flask import Flask, render_template, request, redirect, url_for
+import json
+
+# 自身の名称を app という名前でインスタンス化する
+app = Flask(__name__)
+Bower(app)
+
+# ここからウェブアプリケーション用のルーティングを記述
+# index にアクセスしたときの処理
+@app.route('/')
+def index():
+    title = "将棋盤 pic_to_kif Thank you!"
+    f = open("sample.txt",encoding='utf-8')
+    message = ''.join(f.readlines())
+    f.close()
+    # index.html をレンダリングする
+    return render_template('index.html',message=message)
+
+if __name__ == '__main__':
+    # app.debug = True # デバッグモード有効化
+    app.run(debug=True) # どこからでもアクセス可能に
